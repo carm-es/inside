ab3d9cd0581fd329c87b43437f4612c1d78faba5
jgl13r <jagarcialop3z@gmail.com>
2018-02-02 09:12:28 +0100

    altaDocumentoEni (autogenerar) + altaExpedienteEni (autogen y vacío)
    Extensiones configurables para modificar comportamiento WS-Inside.
    - altaDocumentoEni: permitir identificador autogenerado.
    - altaExpedienteEni: permitir identificador autogenerado y sin documento
    asociado (definiendo en el indice de contenido un documento vacío).
    
    En fichero:
    file:${config.path}/enhanced_2-0-7-x.properties
    Propiedades:
    ws.inside.altaDocumentoEni.autogenerar.identificador.activo=<valor_activacion_S|N>
    ws.inside.altaExpedientEni.autogenerar.identificador.activo=<valor_activacion_S|N>
    ws.inside.altaExpedientEni.documentoIndizado.acepta.vacio.activo=<valor_activacion_S|N>

---
+++ fuentes/inside-web/src/main/java/es/mpt/dsic/inside/ws/config/InsideEnhancedConf.java
@@ -0,0 +1,46 @@
+package es.mpt.dsic.inside.ws.config;
+
+//CARM ### v2.0.7.1
+public class InsideEnhancedConf {
+
+	private static String activoCadena = "S";
+	
+	private String altaDocumentoEniAutogenerarIdentificadorActivo="N";
+	
+	private String altaExpedientEniAutogenerarIdentificadorActivo="N";
+	
+	private String altaExpedientEniDocumentoIndizadoAceptaVacioActivo="N";
+
+	public String getAltaDocumentoEniAutogenerarIdentificadorActivo() {
+		return altaDocumentoEniAutogenerarIdentificadorActivo;
+	}
+	public void setAltaDocumentoEniAutogenerarIdentificadorActivo(String altaDocumentoEniAutogenerarIdentificadorActivo) {
+		this.altaDocumentoEniAutogenerarIdentificadorActivo = altaDocumentoEniAutogenerarIdentificadorActivo;
+	}
+	public boolean isActivoAltaDocumentoEniAutogenerarIdentificador() {
+		return this.altaDocumentoEniAutogenerarIdentificadorActivo.equals(activoCadena);
+	}
+
+	public String getAltaExpedientEniAutogenerarIdentificadorActivo() {
+		return altaExpedientEniAutogenerarIdentificadorActivo;
+	}
+	public void setAltaExpedientEniAutogenerarIdentificadorActivo(String altaExpedientEniAutogenerarIdentificadorActivo) {
+		this.altaExpedientEniAutogenerarIdentificadorActivo = altaExpedientEniAutogenerarIdentificadorActivo;
+	}
+	public boolean isActivoAltaExpedientEniAutogenerarIdentificador() {
+		return this.altaExpedientEniAutogenerarIdentificadorActivo.equals(activoCadena);
+	}
+
+	public String getAltaExpedientEniDocumentoIndizadoAceptaVacioActivo() {
+		return altaExpedientEniDocumentoIndizadoAceptaVacioActivo;
+	}
+	public void setAltaExpedientEniDocumentoIndizadoAceptaVacioActivo(
+			String altaExpedientEniDocumentoIndizadoAceptaVacioActivo) {
+		this.altaExpedientEniDocumentoIndizadoAceptaVacioActivo = altaExpedientEniDocumentoIndizadoAceptaVacioActivo;
+	}
+	public boolean isActivoAltaExpedientEniDocumentoIndizadoAceptaVacio() {
+		return this.altaExpedientEniDocumentoIndizadoAceptaVacioActivo.equals(activoCadena);
+	}
+	
+}
+//CARM 2.0.7.1 ###
