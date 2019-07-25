cf71971021e13c6a49afdb7bb900b4843ffac8ca
Ignacio Barrancos <ignacio.barrancos@carm.es>
2019-07-25 10:43:54 +0200

    Re-aplico el parche b27887d Activación/desactivación detección adicional en ASCII tipo mime x Aplicamos el parche https://github.com/carm-es/inside/commit/b27887d7b42e497a44e9946668c6116bb3d865f5 sobre los fuentes originales.
    La modificación del fichero inside-ws-context.xml, había sido aplicada en el commit anterior

---
+++ fuentes/inside-model/src/main/java/es/mpt/dsic/inside/model/config/ModelEnhancedConf.java
@@ -0,0 +1,34 @@
+package es.mpt.dsic.inside.model.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+// CARM ### v2.0.7.1
+public class ModelEnhancedConf implements ApplicationContextAware {
+
+  private static final String BEAN_NAME = "modelEnhancedConf";
+  private static ApplicationContext ac;
+
+  private boolean magicMatchNotFoundExceptionTryAscii = false;
+
+  @SuppressWarnings("static-access")
+  @Override
+  public void setApplicationContext(ApplicationContext ac) throws BeansException {
+    this.ac = ac;
+  }
+
+  public boolean isMagicMatchNotFoundExceptionTryAscii() {
+    return this.magicMatchNotFoundExceptionTryAscii;
+  }
+
+  public void setMagicMatchNotFoundExceptionTryAscii(boolean magicMatchNotFoundExceptionTryAscii) {
+    this.magicMatchNotFoundExceptionTryAscii = magicMatchNotFoundExceptionTryAscii;
+  }
+
+  public static boolean isActivoMagicMatchNotFoundExceptionTryAscii() {
+    return ((ModelEnhancedConf) ac.getBean(BEAN_NAME)).magicMatchNotFoundExceptionTryAscii;
+  }
+
+}
+// CARM 2.0.7.1 ###
