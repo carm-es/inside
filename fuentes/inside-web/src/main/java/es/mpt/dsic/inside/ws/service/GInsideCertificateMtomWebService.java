6cefc5dcc29e9c2feada6d19c78751cfdcc2fa33
ja-garcia <jagarcialop3z@gmail.com>
2019-08-16 12:57:38 +0100

    Re-aplico el parche beda87b
    CXF Logging: ajustes para que no trunque mensajes largos
    Aplicamos el parche https://github.com/carm-es/inside/commit/beda87bd17dd7ca94cc3e7034a1579b34758a57b sobre los fuentes originales.

--- fuentes/inside-web/src/main/java/es/mpt/dsic/inside/ws/service/GInsideCertificateMtomWebService.java
+++ fuentes/inside-web/src/main/java/es/mpt/dsic/inside/ws/service/GInsideCertificateMtomWebService.java
@@ -42,7 +42,7 @@ import es.mpt.dsic.inside.xml.inside.ws.visualizacion.documento.mtom.TipoResulta
 
 @WebService(name = "GInsideCertificateMtomWebService",
     targetNamespace = "https://ssweb.seap.minhap.es/Inside/XSD/v1.0/WebServiceFiles")
-@Logging
+@Logging(limit = -1)
 @GZIP
 @OutInterceptors(interceptors = {}, classes = {InsideWsIndiceExpedienteSignerInterceptor.class})
 @OutFaultInterceptors(interceptors = {}, classes = {InsideWsErrorInterceptor.class})
