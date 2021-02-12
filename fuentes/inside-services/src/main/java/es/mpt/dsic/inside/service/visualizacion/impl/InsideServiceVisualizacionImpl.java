58808cce32ce30429f230dc92bab81272890a1ed
ja-garcia <jagarcialop3z@gmail.com>
2021-02-12 12:36:26 +0000

    refactor(cxf): config timeouts clientes Eeutils
    Parametrización timeouts clientes servicios web de Eeutils mediante
    propiedades enhanced.
    
    Incluye refactorización de a4e28a879b26ae64f394bdacb616b3d56ec87086
    para eliminar duplicidcades de código.
    
    En fichero:
    file:${config.path}/enhanced_carm.properties
    Propiedades y posibles valores:
    eeutils.cxf.client.connectionTimeout
       =<valor_numérico_entero_positivo_en_milisegundos>
    eeutils.cxf.client.receiveTimeout
       =<valor_numérico_entero_positivo_en_milisegundos>
    
    Closes #67, refactor #65.

--- fuentes/inside-services/src/main/java/es/mpt/dsic/inside/service/visualizacion/impl/InsideServiceVisualizacionImpl.java
+++ fuentes/inside-services/src/main/java/es/mpt/dsic/inside/service/visualizacion/impl/InsideServiceVisualizacionImpl.java
@@ -24,15 +24,13 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
-import org.apache.cxf.interceptor.LoggingInInterceptor;
-import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 import es.mpt.dsic.eeutil.client.model.DatosFirmados;
 import es.mpt.dsic.eeutil.client.operFirma.model.ResultadoValidacionInfo;
+import es.mpt.dsic.eeutil.client.util.CxfClientUtil;
 import es.mpt.dsic.eeutil.client.visDocExp.model.ApplicationLogin;
 import es.mpt.dsic.eeutil.client.visDocExp.model.DocumentoContenido;
 import es.mpt.dsic.eeutil.client.visDocExp.model.DocumentoEniConMAdicionales;
@@ -89,6 +87,10 @@ public class InsideServiceVisualizacionImpl implements InsideServiceVisualizacio
   private boolean eeutilsLoggingCxfEnabled;
   @Value("${eeutils.cxf.client.logging.interceptor.limit:0}")
   private Integer eeutilsLoggingCxfLimit;
+  @Value("${eeutils.cxf.client.connectionTimeout:#{null}}")
+  private Long eeutilsCxfConnectionTimeout;
+  @Value("${eeutils.cxf.client.receiveTimeout:#{null}}")
+  private Long eeutilsCxfReceiveTimeout;
   private EeUtilService port;
   private ApplicationLogin applicationLogin;
 
@@ -138,7 +140,10 @@ public class InsideServiceVisualizacionImpl implements InsideServiceVisualizacio
 
           port = ss.getEeUtilServiceImplPort();
 
-          enableLoggingCxf(ClientProxy.getClient(port));
+          CxfClientUtil.setupLoggingCxf(ClientProxy.getClient(port), eeutilsLoggingCxfEnabled,
+              eeutilsLoggingCxfLimit);
+          CxfClientUtil.setupTimeouts(ClientProxy.getClient(port), eeutilsCxfConnectionTimeout,
+              eeutilsCxfReceiveTimeout);
 
           applicationLogin = new ApplicationLogin();
           applicationLogin.setIdaplicacion(properties.getProperty("visualizacion.idaplicacion"));
@@ -182,15 +187,6 @@ public class InsideServiceVisualizacionImpl implements InsideServiceVisualizacio
       }
     }
     return activo;
-  }
-
-  private void enableLoggingCxf(Client client) {
-    if (eeutilsLoggingCxfEnabled) {
-      client.getInInterceptors().add(eeutilsLoggingCxfLimit == 0 ? new LoggingInInterceptor()
-          : new LoggingInInterceptor(eeutilsLoggingCxfLimit));
-      client.getOutInterceptors().add(eeutilsLoggingCxfLimit == 0 ? new LoggingOutInterceptor()
-          : new LoggingOutInterceptor(eeutilsLoggingCxfLimit));
-    }
   }
 
   @Override
